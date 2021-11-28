029f3d38387a73e04bd5af3f83e4f161b160226d
abdulrahman <hamonyamien@gmail.com>
2021-11-28 17:27:57 +0530

    Updates

---
+++ precache-manifest.0561753061afbe09a08b58a266197525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a803fa7ad19d5b6fb15a32c618c1dc",
+    "url": "/poimaindha/index.html"
+  },
+  {
+    "revision": "96f09fa3a0f632dd0071",
+    "url": "/poimaindha/static/css/main.460af9cc.chunk.css"
+  },
+  {
+    "revision": "3c2a101e076b6825b773",
+    "url": "/poimaindha/static/js/2.21ff3054.chunk.js"
+  },
+  {
+    "revision": "02c9463279c417d23c320e7fdfefb522",
+    "url": "/poimaindha/static/js/2.21ff3054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f37537216a6b15cf2f",
+    "url": "/poimaindha/static/js/3.02db528f.chunk.js"
+  },
+  {
+    "revision": "96f09fa3a0f632dd0071",
+    "url": "/poimaindha/static/js/main.ab053ed7.chunk.js"
+  },
+  {
+    "revision": "6d8f8f4adb799756aa31",
+    "url": "/poimaindha/static/js/runtime-main.89216497.js"
+  }
+]);
